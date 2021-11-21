bdb52ebb59731556e53840443b8cb595d8c06f17
Dan Ramanayake <dananjaya@cynetis-embedded.com>
2021-11-21 16:56:00 +0100

    update instructions to push to docker hub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,5 +5,5 @@ if [ "$TRAVIS_BRANCH" = "main" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG:$TAG
+docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG .
+docker push $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG
--- Readme.md
+++ Readme.md
@@ -3,3 +3,9 @@ docker build -t bobmortimer_names .
 docker run -p 8000:8000 --rm -it -d bobmortimer_names
 
 http://127.0.0.1:8000/v1/name
+
+docker login -u $USER -p $PASSWORD
+
+build -f Dockerfile -t dananjaya/bobmortimer_names:latest .
+
+docker push dananjaya/bobmortimer_names:latest
