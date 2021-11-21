67f4cb374867d9ac0f17a753bf56a976ad238f90
Dan Ramanayake <dananjaya@cynetis-embedded.com>
2021-11-21 13:04:00 +0100

    Initial commit

---
+++ tests/test_generator.py
@@ -0,0 +1,11 @@
+import unittest
+
+from generator import choose_random_name
+
+def test_random_choice_from_list():
+    words = ['foo','bar','foobar']
+    assert choose_random_name(words) in words
+
+def test_random_choice_not_in_list():
+    words = ['foo','bar','foobar']
+    assert 'baz' not in choose_random_name(words)
